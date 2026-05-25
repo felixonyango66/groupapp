238279dd3f110d0910456b1803ac475c87656126
phelix <felixonyango208@gmail.com>
2026-05-25 16:04:33 +0300

    Removed exposed credentials

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'felixonyango208@gmail.com'
-EMAIL_HOST_PASSWORD = 'phelixwailer@1122,,'
-
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
